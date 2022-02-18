783b36ffc97b33284e09252e00ae6e68a0ab66f6
Renata-theFirst <kisulja170691@gmail.com>
2022-02-18 11:06:35 +0300

    add generator

--- iteration5.js
+++ iteration5.js
@@ -90,3 +90,16 @@ docs.forEach((item) => {
     new DocCard(...item).render();
 })
 
+// Генераторы
+// const str = ['s', 'k', 'r', 'i', 'p', 't'];
+const str = 'skript';
+
+function* strVal(str) {
+    for (let i=0; i<=str.length; i++) {
+        yield i++;
+    }
+}
+
+for(let elem of str){
+    console.log(elem);
+}
